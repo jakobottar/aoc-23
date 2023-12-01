48ea02b04032d34eb142015522b790d6fd296982
jakobottar <jakobottar@pm.me>
2023-12-01 11:13:00 -0700

    formatting

--- 01/main.rkt
+++ 01/main.rkt
@@ -9,17 +9,14 @@
 
 (define (cv2 line)
     (cv (regexp-replaces line '([#rx"one" "o1e"]
-                                        [#rx"two" "t2o"]
-                                        [#rx"three" "t3e"]
-                                        [#rx"four" "f4r"]
-                                        [#rx"five" "f5e"]
-                                        [#rx"six" "s6x"]
-                                        [#rx"seven" "s7n"]
-                                        [#rx"eight" "e8t"]
-                                        [#rx"nine" "n9e"]))))
-
-
-
+                                [#rx"two" "t2o"]
+                                [#rx"three" "t3e"]
+                                [#rx"four" "f4r"]
+                                [#rx"five" "f5e"]
+                                [#rx"six" "s6x"]
+                                [#rx"seven" "s7n"]
+                                [#rx"eight" "e8t"]
+                                [#rx"nine" "n9e"]))))
 
 (printf "part 2 example: ~a\n" (apply + (map cv2 (file->lines "example2.txt"))))
 (printf "part 2: ~a\n" (apply + (map cv2 (file->lines "input.txt"))))
