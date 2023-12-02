b4db717cd1efaae1a0e368239b2eb8f29acfaab5
jakobottar <jakobottar@pm.me>
2023-12-02 13:08:06 -0700

    make more racket-y

--- 02/main.rkt
+++ 02/main.rkt
@@ -5,7 +5,7 @@
 (define bag '(12 13 14))
 
 (define (count-valid-game line)
-    (let ([id (string->number (second (string-split (first (string-split line ":")) " ")))])
+  (let ([id (string->number (second (string-split (first (string-split line ":")) " ")))])
     (define (helper cubes)
         (cond [(empty? cubes) id]
               [(and (equal? (second (string-split (first cubes) " ")) "red") (> (string->number (first (string-split (first cubes) " "))) (first bag))) 0]
@@ -20,12 +20,17 @@
 
 ;; part 2
 (define (power-of-cubes line)
-    (let ([red-max 0][blue-max 0][green-max 0])
-        (for ([cubes (string-split (string-replace (second (string-split line ":")) ";" ",") ",")])
-            (cond [(equal? (second (string-split cubes " ")) "red") (set! red-max (max red-max (string->number (first (string-split cubes " ")))))]
-                  [(equal? (second (string-split cubes " ")) "blue") (set! blue-max (max blue-max (string->number (first (string-split cubes " ")))))]
-                  [(equal? (second (string-split cubes " ")) "green") (set! green-max (max green-max (string->number (first (string-split cubes " ")))))]))
-        (* red-max blue-max green-max)))
+  (define (helper draws maxes)
+    (if (empty? draws)
+        (apply * maxes)
+        (let ([col (second (string-split (first draws) " "))]
+              [num (string->number (first (string-split (first draws) " ")))])
+          (cond
+            [(equal? col "red") (helper (rest draws) (list-set maxes 0 (max num (first maxes))))]
+            [(equal? col "green") (helper (rest draws) (list-set maxes 1 (max num (second maxes))))]
+            [(equal? col "blue") (helper (rest draws) (list-set maxes 2 (max num (third maxes))))]))))
+
+  (helper (string-split (string-replace (second (string-split line ":")) ";" ",") ",") '(0 0 0)))
 
 (printf "part 2 example: ~a\n" (apply + (map power-of-cubes (file->lines "example.txt"))))
 (printf "part 2: ~a\n" (apply + (map power-of-cubes (file->lines "input.txt"))))
